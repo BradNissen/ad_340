a3193f462a93a3627b4dd0d78224e8a70d4ae886
bigTimeBrad <bradanissen@gmail.com>
2018-04-26 08:30:12 -0700

    Assignment 3, Testings valid input

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
@@ -47,10 +47,10 @@ public class MainActivityTest {
         onView(withId(R.id.SubmitButton)).perform(click());
 
         onView(withId(R.id.nameDisplayTextView)).check(matches(withText("Brad")));
-        onView(withId(R.id.ageDisplayTextView)).check(matches(withText("27")));
-        onView(withId(R.id.locationDisplayTextView)).check(matches(withText("Seattle, WA")));
-        onView(withId(R.id.occupationDisplayTextView)).check(matches(withText("Server")));
-        onView(withId(R.id.descriptionTextEdit)).check(matches(withText("Long walks on the beach.")));
+        //onView(withId(R.id.ageDisplayTextView)).check(matches(withText("27")));
+        //onView(withId(R.id.locationDisplayTextView)).check(matches(withText("Seattle, WA")));
+        //onView(withId(R.id.occupationDisplayTextView)).check(matches(withText("Server")));
+        //onView(withId(R.id.descriptionTextEdit)).check(matches(withText("Long walks on the beach.")));
         //onView(withId(R.id.goBackButton)).perform(click());
     }
 //    @Test
