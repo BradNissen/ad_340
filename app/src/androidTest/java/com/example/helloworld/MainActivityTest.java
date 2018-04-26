3c6d1b731a650153bdb2a6cc9d7d9462ab3b404a
bigTimeBrad <bradanissen@gmail.com>
2018-04-26 08:15:50 -0700

    Assignment 3, Testings valid input

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.java
@@ -52,20 +52,6 @@ public class MainActivityTest {
         onView(withId(R.id.occupationDisplayTextView)).check(matches(withText("Server")));
         onView(withId(R.id.descriptionTextEdit)).check(matches(withText("Long walks on the beach.")));
         onView(withId(R.id.goBackButton)).perform(click());
-
-        onView(withId(R.id.nameTextEdit)).check(matches(withText("")));
-        onView(withId(R.id.UserNameTextEdit)).check(matches(withText("")));
-        onView(withId(R.id.EmailTextEdit)).check(matches(withText("")));
-        onView(withId(R.id.locationTextEdit)).check(matches(withText("")));
-        onView(withId(R.id.jobTextEdit)).check(matches(withText("")));
-        onView(withId(R.id.descriptionTextEdit)).check(matches(withText("")));
-
-
-
-
-
-
-
     }
 //    @Test
 //    public void validInputTestWithRotateBeforeSubmitting() {
