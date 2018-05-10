c3e06bcc0ea55a1f2f3bc83135a8f11483429d8a
bigTimeBrad <bradanissen@gmail.com>
2018-05-09 17:47:44 -0700

    Assignment 5, changes in app level gradle (updates)

--- app/src/main/java/com/example/helloworld/SettingsFragment.java
+++ app/src/main/java/com/example/helloworld/SettingsFragment.java
@@ -23,60 +23,71 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.recycler_view, container, false);
-        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setHasFixedSize(true);
-        // Set padding for Tiles
-        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
-        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-        return recyclerView;
-    }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView picture;
-        public TextView name;
-        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.settings_fragment, parent, false));
-            picture = (ImageView) itemView.findViewById(R.id.tile_picture);
-            name = (TextView) itemView.findViewById(R.id.tile_title);
-        }
-    }
-    /**
-     * Adapter to display recycler view.
-     */
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
-        // Set numbers of List in RecyclerView.
-        private static final int LENGTH = 18;
-        private final String[] mPlaces;
-        private final Drawable[] mPlacePictures;
-        public ContentAdapter(Context context) {
-            Resources resources = context.getResources();
-            mPlaces = resources.getStringArray(R.array.places);
-            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
-            mPlacePictures = new Drawable[a.length()];
-            for (int i = 0; i < mPlacePictures.length; i++) {
-                mPlacePictures[i] = a.getDrawable(i);
-            }
-            a.recycle();
-        }
 
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
-            holder.name.setText(mPlaces[position % mPlaces.length]);
-        }
-
-        @Override
-        public int getItemCount() {
-            return LENGTH;
-        }
+/* this is just dummy data to fill the page so avoid testing*/
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        return view;
     }
 }
+/* Uncommet here to view tiles page*/
+
+
+//    }
+//        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+//                R.layout.recycler_view, container, false);
+//        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+//        recyclerView.setAdapter(adapter);
+//        recyclerView.setHasFixedSize(true);
+//        // Set padding for Tiles
+//        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+//        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+//        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+//        return recyclerView;
+//    }
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        public ImageView picture;
+//        public TextView name;
+//        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+//            super(inflater.inflate(R.layout.settings_fragment, parent, false));
+//            picture = (ImageView) itemView.findViewById(R.id.tile_picture);
+//            name = (TextView) itemView.findViewById(R.id.tile_title);
+//        }
+//    }
+//    /**
+//     * Adapter to display recycler view.
+//     */
+//    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+//        // Set numbers of List in RecyclerView.
+//        private static final int LENGTH = 18;
+//        private final String[] mPlaces;
+//        private final Drawable[] mPlacePictures;
+//        public ContentAdapter(Context context) {
+//            Resources resources = context.getResources();
+//            mPlaces = resources.getStringArray(R.array.places);
+//            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+//            mPlacePictures = new Drawable[a.length()];
+//            for (int i = 0; i < mPlacePictures.length; i++) {
+//                mPlacePictures[i] = a.getDrawable(i);
+//            }
+//            a.recycle();
+//        }
+//
+//        @Override
+//        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
+//            holder.name.setText(mPlaces[position % mPlaces.length]);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return LENGTH;
+//        }
+//    }
+//}
